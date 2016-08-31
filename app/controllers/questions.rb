f2a8b5bab32095d377dfa932d98d094b6f026180
Audrey Kang <hkang10@gmail.com>
2016-08-31 15:45:48 -0500

    add routes to question and new

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,13 +3,24 @@ get '/questions' do
   erb :"/questions/index"
 end
 
+get '/questions/new' do
+
+  erb :"/questions/new"
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
-  votes = @question.votes
-  @vote_count = 0
-  votes.each do |vote|
-    @vote_count += vote.value
-  end
   erb :"/questions/show"
+end
+
+
+post '/questions' do
+  question = Question.new(text: params[:question], user_id: session[:user_id])
+  if question.save
+    redirect "/questions"
+  else
+    @errors = self.errors.full_messages
+    erb :"/questions/new"
+  end
 end
