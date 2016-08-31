f05d24c2ad7107b1e0434c19e75ac59dce7e9378
sayamkim <sayamkim@gmail.com>
2016-08-31 14:55:46 -0500

    registration page

---
+++ app/controllers/users.rb
@@ -0,0 +1,22 @@
+before do
+	@errors = Array.new
+end
+
+get '/users/new' do
+	erb :"users/new"
+end
+
+post '/users' do
+	@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+	if @user.save
+		session[:user_id] = @user.id
+		redirect '/'
+	else
+		@errors = @user.errors.full_messages
+		erb :"/users/new"
+	end
+end
+
+
+
+end
