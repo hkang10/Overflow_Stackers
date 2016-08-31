b90ddd1d1dd4c49877bac10eaa273a1a6c536a8f
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-31 11:09:31 -0500

    Add s to references

--- db/migrate/20160831102553_create_comments.rb
+++ db/migrate/20160831102553_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
   	create_table :comments do |t|
   		t.text :text, null: false
   		t.integer :user_id, null: false
-  		t.reference :commentable, polymorphic: true
+  		t.references :commentable, polymorphic: true
 
   		t.timestamps(null: false)
   	end
