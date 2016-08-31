7d556d23cf41390e9fe8efa26f2e87b3ca3e8556
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-31 15:57:41 -0500

    Create logout button

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,7 +10,7 @@ post '/users' do
 	@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
 	if @user.save
 		session[:user_id] = @user.id
-		redirect '/'
+		redirect '/questions'
 	else
 		@errors = @user.errors.full_messages
 		erb :"/users/new"
