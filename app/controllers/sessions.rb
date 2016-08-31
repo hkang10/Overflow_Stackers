7cab7103a8dcef6315b132276060fa8a64664b41
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-31 14:29:39 -0500

    Create user login

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,5 +3,12 @@ get "/sessions/new" do
 end
 
 post "/sessions" do
-  
+  @user = User.find_by(email: params[:email])
+
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/"
+  else
+    redirect "/"
+  end
 end
